4b624d1e8c78af59c79d0a7c7d5197534d39834d
Rahul Prajapati <112754941+rkpraj1536@users.noreply.github.com>
2026-03-18 11:19:28 +0530

    done

--- Coding chalanges 18 March/SimpleInterest9.py
+++ Coding chalanges 18 March/SimpleInterest9.py
@@ -2,3 +2,4 @@
 p = float(input("Enter Principle: "))
 r = float(input("Enter Rate of interest: "))
 t = float(input("Enter Time: "))
+print("Your Principle",p,"Rate of interest",r,"Time. So SI is "(p*r*t)/100)
