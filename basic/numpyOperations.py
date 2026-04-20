2426729c617c1aeb3e1c7ba127638f02cbc36b0d
Rahul Prajapati <112754941+rkpraj1536@users.noreply.github.com>
2026-04-20 10:03:23 +0530

    numpy operations

---
+++ basic/numpyOperations.py
@@ -0,0 +1,10 @@
+import numpy as np
+numbers=np.array([23,67,44,57,67,80])
+print(numbers)
+# divide each element by 5
+n=numbers/5
+print("Numbers after dividing by 5")
+print(n)
+#elements greater than 50
+print("greater than 50")
+print(numbers>50)
