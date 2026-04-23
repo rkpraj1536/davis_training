716e2b8734afacbfdeb2a65142e34c096eb404e0
Rahul Prajapati <112754941+rkpraj1536@users.noreply.github.com>
2026-04-23 10:42:13 +0530

    done

---
+++ Class_Work/matrixOperation.py
@@ -0,0 +1,27 @@
+import numpy as np
+#first array
+array1=np.array([10,30,40,50])
+#second array
+array2=np.array([30,20,40,11])
+#-------------------------------------
+print("first array : ")
+print (array1)
+print("second array : ")
+print (array2)
+#addition of array
+#use array3 = np.add(array1,array2)
+#------------------------------------
+array3=np.add(array1,array2)
+print("sum of both array : ")
+print(array3)
+print("--------------------------")
+#substraction of array
+#use array4 = np.subtract(array1,array2)
+#------------------------------------
+array4=np.subtract(array1,array2)
+print("subtraction of both array : ")
+print(array4)
+print("--------------------------")
+
+
+
